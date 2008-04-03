36926f98661b38ca51593b1740b0093e0f476c70
ahpoder <ahpoder@cd5ad199-9c3d-0410-9d3f-bdd6d08f3b64>
2008-04-03 17:13:15 +0000

    Adding source files for the project

---
+++ Module3/HotTargui-ProgProj-F08/src/hottargui/framework/TileType.java
@@ -0,0 +1,10 @@
+package hottargui.framework;
+
+/** TileType is a enumeration over the types of tiles available in
+    Targui.
+
+ */
+
+public enum TileType {
+  Settlement, Oasis, Reg, Feshfesh, Erg, Mountain, Saltlake, Saltmine;
+}
---
+++ Module3/HotTargui-ProgProj-F08/src/hottargui/standard/StandardTile.java
@@ -0,0 +1,38 @@
+package	hottargui.standard;
+
+import hottargui.framework.*;
+
+/** Standard tile implementation. */
+
+public class StandardTile implements Tile {
+  
+  protected Position position;
+  protected TileType type;
+  protected PlayerColor player;
+
+  /** number of units on this tile */
+  private int unit;
+
+  public StandardTile(TileType t, PlayerColor p, int r, int c) {
+    position = new Position(r,c);
+    type = t;
+    player = p;
+    unit = 0;
+  }
+
+  public void changeUnitCount( int newValue ) {
+    unit = newValue;
+  }
+  public void changePlayerColor( PlayerColor newColor ) {
+    player = newColor;
+  }
+  public Position getPosition() { return position; }
+  public PlayerColor getOwnerColor() { return player; }
+  public int getUnitCount() { return unit; }
+  public TileType getType() { return type; }
+
+  public String toString() {
+    return "Tile ("+position.getRow()+","+position.getColumn()+") "+
+	   "units="+ unit;
+  }
+}
