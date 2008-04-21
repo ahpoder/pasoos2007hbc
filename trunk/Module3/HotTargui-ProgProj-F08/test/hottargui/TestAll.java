688459d3aea11c40686b7bef728648a9bd9c50d3
ahpoder <ahpoder@cd5ad199-9c3d-0410-9d3f-bdd6d08f3b64>
2008-04-21 19:21:00 +0000

    Opdatere med Board og første test

--- trunk/Module3/HotTargui-ProgProj-F08/test/hottargui/TestAll.java
+++ trunk/Module3/HotTargui-ProgProj-F08/test/hottargui/TestAll.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
   @Suite.SuiteClasses(
-  { hottargui.config.TestAlphaTargui.class
+  { // hottargui.config.TestAlphaTargui.class,
+    hottargui.config.TestBoard.class
   })
   
   public class TestAll {
