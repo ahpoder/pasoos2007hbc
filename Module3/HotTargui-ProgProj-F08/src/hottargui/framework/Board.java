62344037aa270b649ffd9b8efe4d3dd47adfa403
ahpoder <ahpoder@cd5ad199-9c3d-0410-9d3f-bdd6d08f3b64>
2008-04-21 19:26:38 +0000

    Adding iterator

--- Module3/HotTargui-ProgProj-F08/src/hottargui/framework/Board.java
+++ Module3/HotTargui-ProgProj-F08/src/hottargui/framework/Board.java
@@ -1,9 +1,15 @@
 package hottargui.framework;
 
+import java.util.*;
+
 public class Board {
 	public int getPlayerCount() {
 		return 4;
 	}
 
+	public Iterator<? extends Tile> getBoardIterator()
+	{
+		return null;
+	}
 }
 
