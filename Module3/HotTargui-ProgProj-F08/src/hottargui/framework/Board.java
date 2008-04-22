ed304b393bbf8537d2fcceef21c477661e538d06
ahpoder <ahpoder@cd5ad199-9c3d-0410-9d3f-bdd6d08f3b64>
2008-04-22 10:47:02 +0000

    Updating with some Board test and implementation. Also adding two iterations in the report.

--- Module3/HotTargui-ProgProj-F08/src/hottargui/framework/Board.java
+++ Module3/HotTargui-ProgProj-F08/src/hottargui/framework/Board.java
@@ -3,13 +3,39 @@ package hottargui.framework;
 import java.util.*;
 
 public class Board {
+	private Collection<Tile> tiles;
+	
+	public Board(BoardFactory boardFactory)
+	{
+		tiles = boardFactory.create();
+	}
 	public int getPlayerCount() {
 		return 4;
 	}
 
 	public Iterator<? extends Tile> getBoardIterator()
 	{
-		return null;
+		return tiles.iterator();
+	}
+	
+	public Tile getTile( Position p )
+	{
+		Tile res = null;
+		Iterator<? extends Tile> itt = getBoardIterator();
+		while (itt.hasNext())
+		{
+			Tile t = itt.next();
+			if (t.getPosition().equals(p))
+			{
+				res = t;
+				break;
+			}
+		}
+		if (res == null)
+		{
+			throw new IllegalArgumentException("Position not found in collection");
+		}
+		return res;
 	}
 }
 
---
+++ Module3/HotTargui-ProgProj-F08/src/hottargui/framework/BoardFactory.java
@@ -0,0 +1,8 @@
+package hottargui.framework;
+
+
+import java.util.Collection;
+
+public interface BoardFactory {
+	Collection<Tile> create();
+}
