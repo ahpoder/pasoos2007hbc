0b8f921826727cb7e1e5f82e43f97f95334d69f8
ahpoder <ahpoder@cd5ad199-9c3d-0410-9d3f-bdd6d08f3b64>
2008-05-15 19:39:20 +0000

    Implemented PutUnitsStrategy...

--- Module3/HotTargui-ProgProj-F08-Beta/src/hottargui/config/AlphaPutUnitsStrategy.java
+++ Module3/HotTargui-ProgProj-F08-Beta/src/hottargui/config/AlphaPutUnitsStrategy.java
@@ -2,19 +2,19 @@ package hottargui.config;
 
 import hottargui.framework.*;
 
+/*
+* When units are bought the only valid place to put them is on the player's home settlement
+ */
+ 
 public class AlphaPutUnitsStrategy implements PutUnitsStrategy {
-	Tile t;
-	Player p;
-
 
 	public boolean isPutValid(Player p, Tile t) {
-		this.t = t;
-		this.p = p;
-		if (t.getOwnerColor() != p.getColor()){
-			return false;
-		}
-		else{ 
+		// Is player color == tile color and is tile type == settlement?
+		if ((p.getColor() == t.getOwnerColor()) && t.getType() == TileType.Settlement) {
 			return true;
+		}
+		else {
+			return false;
 		}			
 	}
 
