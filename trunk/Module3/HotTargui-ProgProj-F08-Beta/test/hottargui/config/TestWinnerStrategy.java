5bf3c9bf732ae3f88c8dbeebc85ede37a3a811ef
ahpoder <ahpoder@cd5ad199-9c3d-0410-9d3f-bdd6d08f3b64>
2008-05-24 10:11:25 +0000

    Updating project report regarding BetaTargui - not finished yet

--- trunk/Module3/HotTargui-ProgProj-F08-Beta/test/hottargui/config/TestWinnerStrategy.java
+++ trunk/Module3/HotTargui-ProgProj-F08-Beta/test/hottargui/config/TestWinnerStrategy.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.*;
 public class TestWinnerStrategy {
 
 	private StandardGame game;
-//	private Game game;
 	private Position saltminePos;
 	private GameFactory gameFactory;
 	private BetaWinnerStrategy betaWinner;
@@ -22,7 +21,6 @@ public class TestWinnerStrategy {
 		game = new StandardGame();
   	gameFactory = new BetaGameFactory(game);
   	game.setGameFactory(gameFactory);
-//  	((StandardGame)game).setGameFactory(gameFactory);
 		game.newGame();
 
 		betaWinner = new BetaWinnerStrategy(game);  
@@ -32,7 +30,7 @@ public class TestWinnerStrategy {
 
 	@Test
 	public void testWinnerEqualRevenue() {
-		// Red and Green have Eqaul revenue, but Green owns the saltmine. Green is the winner.
+		// Red and Green have equal revenue, but Green owns the saltmine. Green is the winner.
 		
 		StandardTile oasisTile = (StandardTile) game.getTile(new Position (1,1));
 //    System.out.println(oasisTile.getType() + " has economic value " + saltMineTile.getEconomicValue());
@@ -49,7 +47,7 @@ public class TestWinnerStrategy {
 
 	@Test
 	public void testWinnerRed() {
-		// Red is Winner has 2 x Oasis (4 revenue). Other Players have 1 x Reg (3 revenue each)
+		// Red is Winner has 2 x Reg (4 revenue). Other Players have 1 x Oasis (3 revenue each)
 
 		Tile regTile1 = game.getTile(new Position (0,3));
 		((StandardTile)regTile1).changePlayerColor(PlayerColor.Red);
@@ -71,13 +69,13 @@ public class TestWinnerStrategy {
 
 	@Test
 	public void testWinnerGreen() {
-		// Green is Winner has 2 x Oasis (4 revenue). Other Players have 1 x Reg (3 revenue each)
+		// Green is Winner has 2 x Reg (4 revenue). Other Players have 1 x Oasis (3 revenue each)
 
 		Tile regTile1 = game.getTile(new Position (0,3));
 		((StandardTile)regTile1).changePlayerColor(PlayerColor.Green);
 
 		Tile regTile2 = game.getTile(new Position (3,1));
-		((StandardTile)regTile2).changePlayerColor(PlayerColor.Blue);
+		((StandardTile)regTile2).changePlayerColor(PlayerColor.Green);
 
 		Tile oasisTile1 = game.getTile(new Position (2,0));
 		((StandardTile)oasisTile1).changePlayerColor(PlayerColor.Blue);
@@ -88,12 +86,12 @@ public class TestWinnerStrategy {
 		Tile oasisTile3 = game.getTile(new Position (0,4));
 		((StandardTile)oasisTile3).changePlayerColor(PlayerColor.Red);
 
-		assertEquals(PlayerColor.Blue, betaWinner.getWinner());
+		assertEquals(PlayerColor.Green, betaWinner.getWinner());
 	}
 	
 	@Test
 	public void testWinnerBlue() {
-		// Blue is Winner has 2 x Oasis (4 revenue). Other Players have 1 x Reg (3 revenue each)
+		// Blue is Winner has 2 x Reg (4 revenue). Other Players have 1 x Oasis (3 revenue each)
 
 		Tile regTile1 = game.getTile(new Position (0,3));
 		((StandardTile)regTile1).changePlayerColor(PlayerColor.Blue);
@@ -135,47 +133,4 @@ public class TestWinnerStrategy {
 
 		assertEquals(PlayerColor.Yellow, betaWinner.getWinner());
 	}
-	
-/*		
-	@Test
-	public void testWinner() {
-		StandardTile tile = (StandardTile) game.getTile(new Position(2, 2)); //Fesh Fesh,0
-    tile.changePlayerColor(PlayerColor.Red);
-
-    tile = (StandardTile) game.getTile(new Position(2, 3)); //Oasis,3
-    tile.changePlayerColor(PlayerColor.Red);
-
-    tile = (StandardTile) game.getTile(new Position(6, 3)); //Fesh Fesh,0
-    tile.changePlayerColor(PlayerColor.Green);
-
-    tile = (StandardTile) game.getTile(new Position(5, 0)); //Oasis,3
-    tile.changePlayerColor(PlayerColor.Green);
-
-    game.setState(State.endGame);
-
-    //both players should collect the same revenue -> no winner
-    assertEquals(PlayerColor.None, game.getWinner());
-
-    tile = (StandardTile) game.getTile(new Position(3, 1)); //REG,2
-    tile.changePlayerColor(PlayerColor.Green);
-
-    tile = (StandardTile) game.getTile(new Position(1, 1)); //Oasis,3
-    tile.changePlayerColor(PlayerColor.Green);
-
-    //now green player should collect more revenue
-    assertEquals(PlayerColor.Green, game.getWinner());
-
-    tile = (StandardTile) game.getTile(new Position(3, 3)); //Salt mine,5
-    tile.changePlayerColor(PlayerColor.Red);
-
-    //both player shoud collect same amount, but red player owns the salt mine
-    assertEquals(PlayerColor.Red, game.getWinner());
-
-    tile = (StandardTile) game.getTile(new Position(4, 5)); //reg,2
-    tile.changePlayerColor(PlayerColor.Green);
-
-    //green player collect most revenue, but red player owns the salt mine
-    assertEquals(PlayerColor.Green, game.getWinner());
-  }
-*/	
 }
