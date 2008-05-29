b17a9579e345839df3c81bc7a04dc1d5a25d6beb
ahpoder <ahpoder@cd5ad199-9c3d-0410-9d3f-bdd6d08f3b64>
2008-05-29 12:34:16 +0000

    More text to Domain server architecture...

--- trunk/Module3/HotTargui-ProgProj-F08-DomainServer/src/hottargui/client/ClientGUI.java
+++ trunk/Module3/HotTargui-ProgProj-F08-DomainServer/src/hottargui/client/ClientGUI.java
@@ -2,31 +2,110 @@ package hottargui.client;
 
 import hottargui.domain.*;
 import hottargui.framework.*;
-
+/*
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+*/
 import java.rmi.*;
 import java.rmi.server.*;
 import java.net.*;
 
-public class ClientGUI extends JFrame {
-  private StatusListener myListener;
+import java.util.Iterator;
 
-  public ClientGUI() {
-    super();
-    try
-	{
-		myListener = new ClientUpdater();
-	}
-	catch (RemoteException re)
-	{
-		System.out.println(re);
-	}
-  }
+//public class ClientGUI extends JFrame implements Game {
+public class ClientGUI implements Game {
+	private StatusListener myListener;
+	private State gameState;
+	
+	/** The domain game that the gui interacts with */
+	Game game;
 
-  public StatusListener getStatusListener() {
-    return myListener;
-  }
+	public ClientGUI() {
+		super();
+		try
+		{
+			myListener = new ClientUpdater(gameState);
+		}
+		catch (RemoteException re)
+		{
+			System.out.println(re);
+		}
+	}
+
+	public StatusListener getStatusListener() {
+		return myListener;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public Board getBoard() throws RemoteException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PlayerColor getWinner() throws RemoteException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getDieValue() throws RemoteException
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Player getPlayerInTurn() throws RemoteException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Tile getTile(Position p) throws RemoteException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public Iterator<? extends Tile> getBoardIterator() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public State getState() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addGameListener(GameListener observer) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void rollDie() throws RemoteException {
+		// TODO Auto-generated method stub
+	}
+
+	public PlayerColor turnCard() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean buy(int count, Position deploy) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean move(Position from, Position to, int count) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void newGame() throws RemoteException {
+		// TODO Auto-generated method stub
+	}
 }
