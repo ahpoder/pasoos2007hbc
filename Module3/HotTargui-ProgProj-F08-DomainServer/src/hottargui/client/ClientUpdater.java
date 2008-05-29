942570ccd4b4f5396d4bc3d9f14ea03656151bee
ahpoder <ahpoder@cd5ad199-9c3d-0410-9d3f-bdd6d08f3b64>
2008-05-29 12:34:16 +0000

    More text to Domain server architecture...

--- Module3/HotTargui-ProgProj-F08-DomainServer/src/hottargui/client/ClientUpdater.java
+++ Module3/HotTargui-ProgProj-F08-DomainServer/src/hottargui/client/ClientUpdater.java
@@ -1,19 +1,21 @@
 package hottargui.client;
 
 import hottargui.domain.*;
+import hottargui.framework.*;
 
 import java.rmi.*;
 import java.rmi.server.*;
 import java.net.*;
 
 public class ClientUpdater extends UnicastRemoteObject implements StatusListener {
-
-	public ClientUpdater() throws RemoteException
+	private State gameState;
+	public ClientUpdater(State gameState) throws RemoteException
 	{
 	    super();
+		this.gameState = gameState;
 	}
 
 	public void updateChanges(StatusEvent e) throws RemoteException {
-
+		//To do
 	}
 }
